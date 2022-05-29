7cc93b52a9cc3289cca36b412ce315be61f306a0
HYDR00GEN <ovy_nike@yahoo.it>
2022-05-29 21:04:06 +0200

    feat: added CRUDs

--- src/dashboard-service.ts
+++ src/dashboard-service.ts
@@ -3,7 +3,7 @@ import {Content, Dashboard, PrismaClient } from "@prisma/client";
 export class DashboardService{
     constructor(private readonly prisma: PrismaClient){}
 
-        async moveDashboard(dashboardId: string, position: number): Promise<boolean>{
+    async moveDashboard(dashboardId: string, position: number): Promise<boolean>{
             const dashboards = await this.prisma.dashboard.findMany({
                 orderBy: {
                     position: "asc",
@@ -21,6 +21,109 @@ export class DashboardService{
             const [dashboard] = dashboards.splice(oldPosition, 1)
             dashboards.splice(position, 0, dashboard)
             await this.reoderDashboard(dashboards);
+            return true;
+        }
+    //implementing CRUD api    
+    async createContent(dashboardId: string, text: string){
+        const countContent = await this.prisma.content.count({
+            where: {
+                dashboardId: dashboardId,
+            }
+        });
+        return await this.prisma.content.create({
+            data: {
+                position: countContent,
+                text: text,
+                dashboardId: dashboardId,
+            }
+        })
+    }
+
+    async createDashboard(name: string){
+        const countDashboards = await this.prisma.dashboard.count({});
+        return await this.prisma.dashboard.create({
+            data: {
+                position: countDashboards,
+                name: name,
+            }
+        })
+    }
+
+    async deleteDashboard(dashboardId: string){
+        const contentsInDashboard = await this.prisma.content.count({
+            where:{
+                dashboardId: dashboardId
+            }
+        });
+        if(contentsInDashboard > 0){
+            return null
+        }
+        return await this.prisma.dashboard.delete({
+            where: {
+                id: dashboardId,
+            }
+        })
+    }
+
+    async deleteContent(dashboardId: string, contentId: string){
+        return await this.prisma.content.delete({
+            where: {
+                id_dashboardId: {
+                    dashboardId: dashboardId,
+                    id: contentId,
+                }
+            }
+        })
+    }
+
+    async moveContent(
+            contentId: string, 
+            position: number, 
+            fromDashboardId: string, 
+            toDashboardId: string
+            ): Promise<boolean>{
+            const fromToSameDashboard = fromDashboardId == toDashboardId;
+                //list of contents from start
+            const fromContents = await this.prisma.content.findMany({
+                orderBy: {
+                    position: "asc",
+                },
+                where: {
+                    dashboardId: fromDashboardId
+                }
+            });
+
+            const toContents = fromToSameDashboard ? 
+            fromContents : await this.prisma.content.findMany({
+                orderBy: {
+                    position: "asc",
+                },
+                where: {
+                    dashboardId: toDashboardId
+                }
+            });
+
+            //i can't use a position greater than my array's len
+            if(position > toContents.length){
+                return false
+            }
+
+            const oldPosition = fromContents.findIndex(c=> c.id == contentId);
+            if (oldPosition == -1){
+                return false
+            }
+            //return eliminated elements
+            const [content] = fromContents.splice(oldPosition, 1)
+            if(oldPosition<position){
+                position = position -1;
+            }
+
+            toContents.splice(position, 0, content);
+            await this.reoderContent(fromContents, fromDashboardId)
+
+            if(!fromToSameDashboard){
+                await this.reoderContent(toContents, toDashboardId)
+            }
             return true;
         }
 
@@ -52,6 +155,21 @@ export class DashboardService{
             });
         });
         //return all my updates
+        await this.prisma.$transaction(updates)
+    }
+
+    async reoderContent(contents: Content[], dashboardId: string){
+        const updates = contents.map((content, position)=>{
+            return this.prisma.content.update({
+                where: {
+                    id: content.id,
+                },
+                data:{
+                    position: position,
+                    dashboardId: dashboardId,
+                },
+            });
+        });
         await this.prisma.$transaction(updates)
     }
     
--- src/server.ts
+++ src/server.ts
@@ -37,6 +37,57 @@ server.post("/:dashboardId/move", async(req,res)=>{
     res.send(dashboards)
 })
 
+server.post("/:dashboardId/:contentId/move", async(req,res)=>{
+    //specify pos & dash in which arrives
+    const to = req.body;
+    const {dashboardId,  contentId} = req.params;
+    const ok = await dashboardService.moveContent(
+        contentId, 
+        to.position, 
+        dashboardId,
+        to.dashboardId
+    );
+    if(!ok){
+        return res.status(401).send({msg: "unnable to move dash"})
+    }
+
+    const dashboards = await dashboardService.getDashboard();
+    res.send(dashboards)
+})
+
+server.post("/", async(req,res)=>{
+    const {name} = req.body;
+    await dashboardService.createDashboard(name);
+    const dashboards = await dashboardService.getDashboard();
+    res.send(dashboards)
+})
+server.post("/:dashboardId", async(req,res)=>{
+    const {dashboardId} = req.params
+    const {text} = req.body;
+    await dashboardService.createContent(dashboardId, text);
+    const dashboards = await dashboardService.getDashboard();
+    res.send(dashboards)
+})
+
+server.delete("/:dashboardId", async(req,res)=>{
+    const {dashboardId} = req.params
+    const dashboard = await dashboardService.deleteDashboard(dashboardId);
+    if(!dashboard){
+        return res.status(401).send({msg: "cannot delete dashboard"})
+    }
+    const dashboards = await dashboardService.getDashboard();
+    res.send(dashboards)
+})
+
+server.delete("/:dashboardId/:contentId", async(req,res)=>{
+    const {dashboardId, contentId} = req.params
+    const content = await dashboardService.deleteContent(dashboardId, contentId);
+    if(!content){
+        return res.status(401).send({msg: "cannot delete content"})
+    }
+    const dashboards = await dashboardService.getDashboard();
+    res.send(dashboards)
+})
 
 //enabling soft closing serverr
 process.on("SIGTERM",async()=> {
